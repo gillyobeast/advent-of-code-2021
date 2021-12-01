f99c3f7d2d724581a3764f963db018b561fd7d61
Luke Armitage <luke.armitage@sky.uk>
2021-12-01 19:50:26 +0000

    add test input and comments with strategy

--- src/advent_of_code_2021/core.clj
+++ src/advent_of_code_2021/core.clj
@@ -3,5 +3,13 @@
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  []
   (println "Hello, World!"))
+
+(def input
+  "Test input with 3 increases."
+  [100 101 102 101 102 101 100])
+
+;; write a function that compares two depths, 
+;;    and returns 1 if there is an increase
+;; then reduce that function over the input with initial of 0
